568972949b85c52492850354d11e59436a7ed09c
I=m-abhi <abhisasa2001@gmail.com>
2024-01-24 23:39:11 +0530

    Basic of recat

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// const jsxHeading = <h1>JSX Haeding</h1>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(jsxHeading);
+
+//Function Based Components
+const HeadingComponent = ()=>(
+  <div id="container">
+    <h1>React Functional Component</h1>
+  </div>
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent/>);
+
+
 // Adding HTML Element through React.js
 // const heading = React.createElement('h1', { id: 'heading' }, 'Hello from react.js');
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,15 +32,16 @@
 //       <h2> I am a h2 heading tag </h2>
 //     </div>
 //  </div>
-const parent = React.createElement('div',{id : 'parent'},
-  [React.createElement('div',{id : 'child'},
-  [React.createElement('h1',{},'I am a h1 heading tag'),React.createElement('h2',{},'I am a h2 heading tag')]
-  ),
-  React.createElement('div',{id : 'child2'},
-  [React.createElement('h1',{},'I am a h1 heading tag'),React.createElement('h2',{},'I am a h2 heading tag')]
-  )]
-);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const parent = React.createElement('div',{id : 'parent'},
+//   [React.createElement('div',{id : 'child'},
+//   [React.createElement('h1',{},'I am a h1 heading tag'),React.createElement('h2',{},'I am a h2 heading tag')]
+//   ),
+//   React.createElement('div',{id : 'child2'},
+//   [React.createElement('h1',{},'I am a h1 heading tag'),React.createElement('h2',{},'I am a h2 heading tag')]
+//   )]
+// );
 
-root.render(parent);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(parent);
